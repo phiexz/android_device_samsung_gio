2df7043fedf1491b4cc3dfeaaed8bc7b76fee1a8
Andika Edo <dhiika@phiexz.com>
2012-07-03 08:04:30 +0700

    remove build sec_touchscreen

--- Android.mk
+++ Android.mk
@@ -17,11 +17,6 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),gio)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := sec_touchscreen.kcm
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_KEY_CHAR_MAP)
-
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := setup_fs.c
 LOCAL_MODULE := setup_fs
 LOCAL_MODULE_TAGS := optional
